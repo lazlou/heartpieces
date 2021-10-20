6dd22cb7df78097b835f4bd49ea4535936bdbf25
lazzy <leslieharka@gmail.com>
2021-10-20 00:14:50 -0700

    homepage typed text fixed

--- typedtext.js
+++ typedtext.js
@@ -1,4 +1,4 @@
-const typedTextSpan = document.querySelector("#typed-text");
+const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
 const textArray = ["heart pieces"];
@@ -8,76 +8,104 @@ let textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-  if (charIndex < textArray[textArrayIndex].length) 
-  {
+  if (charIndex < textArray[textArrayIndex].length) {
     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
   } 
   else {
     cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+
+// const typedTextSpan = document.querySelector("#typed-text");
+// const cursorSpan = document.querySelector(".cursor");
+
+// const textArray = ["heart pieces"];
+// const typingDelay = 200;
+// const newTextDelay = 2000; // Delay between current and next text
+// let textArrayIndex = 0;
+// let charIndex = 0;
+
+// function type() {
+//   if (charIndex < textArray[textArrayIndex].length) 
+//   {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+//     charIndex++;
+//     setTimeout(type, typingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//   }
+// }
 
 
 
 
 // ----------
 
-let texts = {
-  first: "frst text",
-  second: "second",
-  third: "third"
-}
+// let texts = {
+//   first: "frst text",
+//   second: "second",
+//   third: "third"
+// }
 
-let indices = {
-  first: 0,
-  second: 0,
-  third: 0,
-}
+// let indices = {
+//   first: 0,
+//   second: 0,
+//   third: 0,
+// }
 
-function startType(typeel){
-  console.log('typeel', typeel)
-  let element_id = typeel.parentElement.parentElement.id;
-  let our_text = texts[element_id];
-  let our_idx = indices[element_id];
-  console.log(element_id, our_text, our_idx);
+// function startType(typeel){
+//   console.log('typeel', typeel)
+//   let element_id = typeel.parentElement.parentElement.id;
+//   let our_text = texts[element_id];
+//   let our_idx = indices[element_id];
+//   console.log(element_id, our_text, our_idx);
 
-  if (our_idx < our_text.length){
-    typeel.textContent += our_text[our_idx]
-    indices[element_id] += 1;
-    setTimeout(function(){startType(typeel)}, typingDelay);
-  }
-
-
-}
+//   if (our_idx < our_text.length){
+//     typeel.textContent += our_text[our_idx]
+//     indices[element_id] += 1;
+//     setTimeout(function(){startType(typeel)}, typingDelay);
+//   }
 
 
+// }
 
 
 
- // ----------
 
-let observers = [];
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
+
+//  // ----------
+
+// let observers = [];
+// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+//   if(textArray.length) setTimeout(type, newTextDelay + 250);
   
-  let heartText = document.getElementsByClassName("typed-heartpieces");
-  for (let i = 0; i < heartText.length; i++) {
+//   let heartText = document.getElementsByClassName("typed-heartpieces");
+//   for (let i = 0; i < heartText.length; i++) {
 
-    // let options = {
-    //   root: heartText[i],
-    //   rootMargin: '0px',
-    //   threshold: 1.0
-    // }
+//     // let options = {
+//     //   root: heartText[i],
+//     //   rootMargin: '0px',
+//     //   threshold: 1.0
+//     // }
 
-    // observers[i] = new IntersectionObserver(function(){startType(heartText[i])}, options);
-    startType(heartText[i]);
-   }
-  console.log(heartText);
+//     // observers[i] = new IntersectionObserver(function(){startType(heartText[i])}, options);
+//     startType(heartText[i]);
+//    }
+//   console.log(heartText);
 
   
-});
+// });
 
 
